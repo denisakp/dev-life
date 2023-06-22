e340422eb0448b8bfc8520044d0b6391f4efeae6
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-06-22 18:26:09 +0100

    added data

---
+++ data/projects.ts
@@ -0,0 +1,11 @@
+const projects = [
+    {
+        "title": "DenisAKP (Site web)",
+        "description": "Site web où je partage des articles portant sur l'ingénierie logicielle, la philosophie du DevOps et le cloud Infrastructure ",
+        "date": "Décembre 2022",
+        "tags": ["nuxt3", "nuxt-content", "digitalocean"],
+        "preview": "https://denisakp.me",
+        "github": "https://github.com/denisakp/myBlogSite"
+    }
+];
+export default projects;
