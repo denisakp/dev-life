3f9aa2336e273250f9fbc235cd2ab34dbc0e3e56
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-06-24 22:27:37 +0100

    added sitemap configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,29 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/image-edge',
-    '@nuxt/content',
-    '@nuxtjs/color-mode'
-  ],
-  colorMode: {
-    classSuffix: ''
-  },
-  content: {
-    highlight: {
-      theme: {
-        default: 'material-default',
-        dark: 'material-darker',
-      }
+    devtools: {enabled: true},
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxt/image-edge',
+        '@nuxt/content',
+        '@nuxtjs/color-mode',
+        'nuxt-simple-sitemap',
+    ],
+    colorMode: {
+        classSuffix: ''
     },
-  },
-
-  // @ts-ignore
-  ssr: true,
-  nitro: {
-    compressPublicAssets: true,
-  },
+    content: {
+        highlight: {
+            theme: {
+                default: 'material-default',
+                dark: 'material-darker',
+            }
+        },
+    },
+    // @ts-ignore
+    ssr: true,
+    nitro: {
+        compressPublicAssets: true,
+    },
+    sitemap: {
+        siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://denisakp.me'
+    }
 })
