0e633921b658233841665eaba01ee82b69559cfb
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-07-05 04:00:22 +0100

    updated nuxt configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,28 @@
 export default defineNuxtConfig({
-    devtools: {enabled: true},
+    devtools: {enabled: false},
     modules: [
         '@nuxtjs/tailwindcss',
         '@nuxt/image-edge',
         '@nuxt/content',
         '@nuxtjs/color-mode',
         'nuxt-simple-sitemap',
+        '@nuxtjs/google-fonts'
     ],
+    // @ts-ignore
+    css: [
+        '@/assets/styles/main.css',
+        '@/assets/styles/typography.css'
+    ],
+    // @ts-ignore
+    extends: ['@nuxt-themes/typography'],
     colorMode: {
         classSuffix: ''
     },
     content: {
         highlight: {
             theme: {
-                default: 'material-default',
-                dark: 'material-darker',
+                default: 'github-light',
+                dark: 'github-dark',
             }
         },
     },
@@ -22,8 +30,25 @@ export default defineNuxtConfig({
     ssr: true,
     nitro: {
         compressPublicAssets: true,
+        prerender: {
+            crawlLinks: true,
+            routes: ['/', '/sitemap.xml']
+        }
     },
     sitemap: {
-        siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://denisakp.me'
+        siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://denisakp.me',
+        // @ts-ignore
+        changefreq: 'weekly'
+    },
+    googleFonts: {
+        display: 'swap',
+        families: {
+            Inter: [400, 500, 700],
+        }
+    },
+    runtimeConfig: {
+        public: {
+            siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://denisakp.me'
+        }
     }
 })
