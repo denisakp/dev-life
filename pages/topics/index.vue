c4c008e339c9daf1bea0f20fdcb838e016a52070
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-07-05 04:00:07 +0100

    code review

--- pages/topics/index.vue
+++ pages/topics/index.vue
@@ -18,11 +18,9 @@ useSeoMeta({
 </script>
 
 <template>
-  <div>
-    <div class="page-bg">
-      <div class="container">
-        <h3>LES SUJETS {{ topics.length }} </h3>
-      </div>
+  <div class="page-bg">
+    <div class="container">
+      <h3>LES SUJETS {{ topics.length }} </h3>
     </div>
   </div>
 </template>
