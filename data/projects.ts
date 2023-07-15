18c2cba4015242ded2fb1a9cb63ec16ab60685d4
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-07-15 12:49:34 +0100

    added squareHR url shortener project

--- data/projects.ts
+++ data/projects.ts
@@ -2,7 +2,7 @@ const projects = [
     {
         "title": "Dev Life",
         "description": "Dev Life, mon blog de dev, où je partage des articles portant sur l'ingénierie des logiciels(architecture, développement et tests), la philosophie du DevOps, l'Intelligence Artificielle et la Cybersécurité ",
-        "date": "Décembre 2022",
+        "date": "December 2022",
         "tags": ["nuxt3", "nuxt-content", "vercel"],
         "preview": "https://denisakp.me",
         "github": "https://github.com/denisakp/denisakp.github.io"
@@ -10,10 +10,17 @@ const projects = [
     {
         "title": "Fake Store API",
         "description": "Une API REST qui fournit des données de commerce électronique fictives au format JSON aux développeurs qui souhaitent rapidement créer un prototype d'application de commerce électronique web ou mobile.",
-        "date": "Juin 2023",
+        "date": "May 2023",
         "tags": ["api-rest", "nextjs", 'mongodb'],
         "preview": "https://fakestoreapi.denisakp.me",
         "github": "https://github.com/denisakp/fake-store-api"
+    },
+    {
+        "title": "URL Shortener",
+        "description": "Une API REST qui fournit des données de commerce électronique fictives au format JSON aux développeurs qui souhaitent rapidement créer un prototype d'application de commerce électronique web ou mobile.",
+        "date": "July 2023",
+        "tags": ["laravel", "inertiajs", 'vuejs'],
+        "github": "https://github.com/denisakp/SquaHR_url_shortener"
     },
 ];
 export default projects;
