8994678f8e0ed78fb5a47c20faab13dc390833cc
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-08-22 20:36:50 +0100

    added preview to fake store api project

--- data/projects.ts
+++ data/projects.ts
@@ -1,27 +1,28 @@
 const projects = [
-  {
-    title: "Dev Life",
-    description:
-      "Welcome to Dev Life - your one-stop destination for all things software engineering, Cloud DevOps, AI, and cyber security. Join us on a journey through cutting-edge technologies and industry insights.",
-    date: "December 2022",
-    tags: ["nuxt3", "nuxt-content", "vercel"],
-    github: "https://github.com/denisakp/denisakp.github.io",
-  },
-  {
-    title: "Fake Store API",
-    description:
-      "Fake Store API is a REST API that provides fake e-commerce data in JSON format to developers who want to quickly prototype a web or mobile e-commerce application .",
-    date: "May 2023",
-    tags: ["api-rest", "nextjs", "mongodb"],
-    github: "https://github.com/denisakp/fake-store-api",
-  },
-  {
-    title: "URL Shortener",
-    description:
-      "By combining Laravel, MySQL, Vue.js, Inertia, and Laravel Sail, I have developed a URL shortener application with a robust backend, efficient data management using MySQL, an interactive user interface powered by Vue.js.",
-    date: "July 2023",
-    tags: ["laravel", "inertiajs", "vuejs"],
-    github: "https://github.com/denisakp/SquaHR_url_shortener",
-  },
+    {
+        title: "Dev Life",
+        description:
+            "Your one-stop destination for all things Software Engineering, Cloud DevOps, AI, and Cyber security. Join us on a journey through cutting-edge technologies and industry insights.",
+        date: "December 2022",
+        tags: ["nuxt3", "nuxt-content", "vercel"],
+        github: "https://github.com/denisakp/denisakp.github.io",
+    },
+    {
+        title: "Fake Store API",
+        description:
+            "Fake Store API is a REST API that provides fake e-commerce data in JSON format to developers who want to quickly prototype a web or mobile e-commerce application .",
+        date: "May 2023",
+        tags: ["api-rest", "nextjs", "mongodb"],
+        github: "https://github.com/denisakp/fake-store-api",
+        preview: "https://fake-store-api-sigma.vercel.app"
+    },
+    {
+        title: "URL Shortener",
+        description:
+            "By combining Laravel, MySQL, Vue.js, Inertia, and Laravel Sail, I have developed a URL shortener application with a robust backend, efficient data management using MySQL, an interactive user interface powered by Vue.js.",
+        date: "July 2023",
+        tags: ["laravel", "inertiajs", "vuejs"],
+        github: "https://github.com/denisakp/SquaHR_url_shortener",
+    },
 ];
 export default projects;
