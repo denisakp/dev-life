5b90c00e711b82c25cdb36317b1922f392391cb9
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-08-23 12:25:58 +0100

    added postImage component

---
+++ components/content/PostImage.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  source: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <nuxt-img class="w-full object-contain" :src="source" :alt="alt"/>
+</template>
